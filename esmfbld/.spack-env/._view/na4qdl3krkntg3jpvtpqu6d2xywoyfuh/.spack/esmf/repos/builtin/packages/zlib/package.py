028ea519cd14b13563627373de76e992e09bf737
James Edwards <jedwards@ucar.edu>
2022-10-05 09:00:39 -0600

    my spack environments on gust

---
+++ esmfbld/.spack-env/._view/na4qdl3krkntg3jpvtpqu6d2xywoyfuh/.spack/esmf/repos/builtin/packages/zlib/package.py
@@ -0,0 +1,1 @@
+/glade/work/jedwards/sandboxes/spack/opt/spack/cray-sles15-zen3/intel-2022.1.0/esmf-8.4.0b15-j6zxleyihqfclnisfedsgi5jbjaimbib/.spack/repos/builtin/packages/zlib/package.py
---
+++ esmfbld/.spack-env/._view/na4qdl3krkntg3jpvtpqu6d2xywoyfuh/include/pionfatt_mod.mod
@@ -0,0 +1,1 @@
+/glade/work/jedwards/sandboxes/spack/opt/spack/cray-sles15-zen3/intel-2022.1.0/parallelio-2.5.9-k7h6vhittnl2frpvuut7tmfkmkh6xwgc/include/pionfatt_mod.mod
